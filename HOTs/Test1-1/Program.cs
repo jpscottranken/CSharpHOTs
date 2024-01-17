b8636a5475378bf2f6d4f0d19899776ea04cd08b
Jeff Scott <jpscott@ranken.edu>
2024-01-17 10:41:10 -0600

    HOTs

---
+++ HOTs/Test1-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace Test1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("C# is fun!");
+            ReadLine();
+        }
+    }
+}
